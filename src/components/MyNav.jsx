bb6882a252a9cdb21f570b0b47fbc1538e160acc
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-03-15 12:14:59 +0100

    first-commit

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,53 @@
+// const url = "http://www.omdbapi.com/?i=tt3896198&apikey=df894da2";
+
+import React from "react";
+import { Navbar, Nav, Container, Form, Button } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import { Search, Bell, PersonCircle } from "react-bootstrap-icons";
+
+function NavBar() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img alt="Netflix Logo" src={logo} width="100" height="55" className="d-inline-block align-top" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" className="fw-bold">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              TV Shows
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              Movies
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              Recently Added
+            </Nav.Link>
+            <Nav.Link href="#" className="fw-bold">
+              My List
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Button className="bg-dark border-0 text-light">
+              <Search />
+            </Button>
+          </Form>
+          <Button className="bg-dark border-0 fw-bold text-light">KIDS</Button>
+          <Button className="bg-dark border-0 text-light">
+            <Bell />
+          </Button>
+          <Button className="bg-dark border-0 text-light">
+            <PersonCircle />
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
---
+++ src/components/TvShow.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ButtonGroup, Dropdown, Button, Container } from "react-bootstrap";
+import { Grid, Grid3x3 } from "react-bootstrap-icons";
+
+function TVShows() {
+  return (
+    <Container fluid className="d-flex justify-content-between bg-dark">
+      <div className="d-flex">
+        <h2 className="mb-4 text-light">TV Shows</h2>
+        <ButtonGroup role="group">
+          <Dropdown className="ms-4 mt-1">
+            <Dropdown.Toggle variant="secondary" size="sm" className="rounded-0" style={{ backgroundColor: "#221f1f" }}>
+              Genres
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#">Comedy</Dropdown.Item>
+              <Dropdown.Item href="#">Drama</Dropdown.Item>
+              <Dropdown.Item href="#">Thriller</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </ButtonGroup>
+      </div>
+      <ButtonGroup>
+        <Button className="bg-dark border-0 text-light">
+          <Grid className="icons" />
+        </Button>
+        <Button className="bg-dark border-0 text-light">
+          <Grid3x3 className="icons" />
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
+}
+
+export default TVShows;
